5f26c6b2a28b1f42ae2d228a250be7f0a128336e
aishrahatal <aishwarya.rahatal97@gmail.com>
2021-11-25 00:05:33 +0530

    [Aishwarya] Add: main for function call

---
+++ EmployeeWage.py
@@ -0,0 +1,12 @@
+'''	
+@Author: Aishwarya
+@Date: 2021-11-24
+@Title : Employee Wage Computation Problem
+'''
+########################################################################################################
+# main for function call.
+if __name__ == "__main__": 
+
+    print("Employee Wage Computation Problem")
+    print("------------------------------------")
+    
